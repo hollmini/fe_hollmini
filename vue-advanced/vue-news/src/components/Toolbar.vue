d609b091a582027859636e26b2fcece754a31307
김민재 <hollmini@hivelab.co.kr>
2019-08-09 16:46:31 +0900

    Router setting

---
+++ vue-advanced/vue-news/src/components/Toolbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+    <router-link to="/news">News</router-link> |
+    <router-link to="/ask">Ask</router-link> |
+    <router-link to="/jobs">Jobs</router-link>
+  </div>
+</template>
+
+<style scoped>
+/* scoped 속성 : 해당하는 컴포넌트에만 css가 적용되도록 함 */
+.header {
+  display: flex;
+  padding: 8px;
+  background-color: #42b883;
+  color: #fff;
+}
+.header a {
+  color: #fff;
+}
+.header .router-link-exact-active {
+  color: #35495e;
+}
+</style>
---
+++ vue-advanced/vue-news/src/routes/index.js
@@ -0,0 +1,42 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import NewsView from '../views/NewsView.vue';
+import AskView from '../views/AskView.vue';
+import JobsView from '../views/JobsView.vue';
+import ItemView from '../views/UserView.vue';
+import UserView from '../views/UserView.vue';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+	mode: 'history', // url hash값 제거
+  routes: [
+		{
+				path: '/',
+				redirect: '/news',
+		},
+	  {
+      // path: url 주소
+      path: '/news',
+      // components: url 주소로 갔을 때 표시될 컴포넌트
+		  component: NewsView,
+	  },
+	  {
+		  path: '/ask',
+		  component: AskView,
+	  },
+	  {
+		  path: '/jobs',
+		  component: JobsView,
+		},
+		{
+			path: '/user',
+			component: UserView,
+		},
+		{
+			path: '/item',
+			component: ItemView
+			,
+		},
+  ]
+});
